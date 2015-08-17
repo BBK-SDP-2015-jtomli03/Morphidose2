5d47784ff509ff9d00bfb95344cb0846b5509ef0
jtomli03 <jotom1000@gmail.com>
2015-08-17 18:35:57 +0100

    First commit - email auth only

---
+++ app/models/daos/DAOSlick.scala
@@ -0,0 +1,12 @@
+package models.daos
+
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfig}
+import slick.driver.JdbcProfile
+
+/**
+ * Trait that contains gimport models.daos.DBTableDefinitions
+ * generic slick db handling code to be mixed in with DAOs
+ */
+trait DAOSlick extends DBTableDefinitions with HasDatabaseConfig[JdbcProfile] {
+  protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile](play.api.Play.current)
+}
